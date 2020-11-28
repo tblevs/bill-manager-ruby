ee1f6a2c946ced9d768a11d426a27a8a39537cbf
Thomas Blevins <tblevs@TB-Development.lan>
2020-11-28 17:54:53 -0500

    We fixed navigation

---
+++ app/controllers/calculator_controller.rb
@@ -0,0 +1,6 @@
+class CalculatorController < ApplicationController
+
+  def index
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  get 'welcome/index'
-  root 'home#index'
+  get 'calculator/index'
+  get 'account/index'
+  get 'expenses/index'
+
+  # get 'bills/:id', to: 'bills#show'
+
+  root 'expenses#index'
 end
